e07f4d741030a2293c118a79a3016f526c2fe7b7
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-03-09 23:03:08 +0100

    Works

--- list2/computation/handlers.py
+++ list2/computation/handlers.py
@@ -1,11 +1,22 @@
 import pathlib
 import pydantic
+import json
 
 
 # TODO: should return dict with config if config is correct, else raise error
 # TODO: use pydantic
-def check_config_integrity():
+def check_config_integrity(file_path: str):
     """
     Check if config file isn't corrupted or in formatted in wrong way
     """
-    pass
+    file_path = pathlib.Path(file_path)
+
+    with file_path.open('r') as file:
+        temp = file
+        config = json.loads(file)
+        for code, orginal in enumerate(list(config['config'].keys())):
+            temp.replace(orginal, code)
+        return json.loads(temp)
+
+    
+    # swap = list(enumerate(list(config['config'].keys())))
