99838796ba7a5bd0373e79911a7cef1c4eb31593
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-03-09 12:01:16 +0100

    Finite state machine base Base for finite-state machine simulation program.

---
+++ list2/computation/handlers.py
@@ -0,0 +1,9 @@
+import pathlib
+
+
+# TODO: should return dict with config if config is correct, else raise error
+def check_config_integrity():
+    """
+    Check if config file isn't corrupted or in formatted in wrong way
+    """
+    pass
---
+++ list2/computation/machine.py
@@ -0,0 +1,17 @@
+import json
+from handlers import check_config_integrity
+
+
+
+class FiniteStateMachine:
+    """
+    Class representing Finite-state machine
+    """
+    def __init__(self, config):
+        pass
+
+    def check_input(self, seqence: str):
+        pass
+
+    def run(self):
+        pass
