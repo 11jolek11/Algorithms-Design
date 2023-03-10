3c25c388e38ea5ad51e0a1cead917dd9e1dd3304
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-03-11 00:15:53 +0100

    TODO update

--- list2/task1/main.py
+++ list2/task1/main.py
@@ -6,6 +6,5 @@ from computation.handlers import config_encoder, config_to_matrix
 if __name__ == "__main__":
     mach = FiniteStateMachine(config=config_to_matrix(config_encoder('./config.json')))
     # mach.decode()
-    # TODO: input encoding
     mach.check_input([0, 0, 1 ])
     mach.run()
