6675d169beb970a3de76cca53577b4d5afbd1790
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-04-10 16:13:24 +0200

    backup rsa

---
+++ list6/list6/p_rsa.py
@@ -0,0 +1,30 @@
+import random
+
+def generate_keypair(p, q):
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = random.randrange(1, phi)
+    gcd = lambda a, b: a if not b else gcd(b, a % b)
+    while gcd(e, phi) != 1:
+        e = random.randrange(1, phi)
+    d = pow(e, -1, phi)
+    return ((e, n), (d, n))
+
+def encrypt(pk, plaintext):
+    key, n = pk
+    cipher = [pow(ord(char), key, n) for char in plaintext]
+    return cipher
+
+def decrypt(pk, ciphertext):
+    key, n = pk
+    plain = [chr(pow(char, key, n)) for char in ciphertext]
+    return ''.join(plain)
+
+p = 61
+q = 53
+public_key, private_key = generate_keypair(p, q)
+message = "Hello, World!"
+encrypted_message = encrypt(public_key, message)
+decrypted_message = decrypt(private_key, encrypted_message)
+
+print(decrypted_message)
