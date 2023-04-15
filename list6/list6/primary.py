50b5a0cf03861875159de2d7a81bc9531dfab730
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-04-16 00:19:43 +0200

    working

--- list6/list6/primary.py
+++ list6/list6/primary.py
@@ -12,20 +12,41 @@ from math import sqrt, floor
 #             find_primary(int(divider/number), orginal_number, result)
 
 
-def find_primary(divider: int, orginal_number: int, result: list=[]):
-    # 
+# def find_primary(divider: int, orginal_number: int, result: list=[]):
+#     for number in range(2, floor(sqrt(divider))+1):
+#         print(f'{number}/{divider}')
+#         if divider % number == 0:
+#             result.append(number)
+#             find_primary(int(divider/number), orginal_number, result)
+#             break
+#     return result
+
+def find_primes(orginal_number, divider, result):
+    print(f'New spawn {orginal_number} {divider} {result}')
     for number in range(2, floor(sqrt(divider))+1):
-        # if divider / number == 1:
-        #     result.append(number)
-        #     return result
-        if divider % number == 0:
+       if orginal_number%number == 0:
+            print(f'{orginal_number}/{number} = {orginal_number/number}')  
             result.append(number)
-            find_primary(int(divider/number), orginal_number, result)
-        # else:
-        #     result.append(divider)
+            return find_primes(orginal_number/number, number, result)
+            # break
     return result
 
 
+def prime_factorize(number):
+    def recurse(factors, x, n):
+        if x<2: return factors 
+        if n >  floor(sqrt(x))+1: 
+            factors.append(x)
+            return factors
+        if x%n==0:
+            factors.append(n)
+            return recurse(factors, x/n, n)
+        else:
+            return recurse(factors, float(x), n+1)
+    return recurse([], number, 2)
+
+
 if __name__ == "__main__":
-    print(find_primary(125, 125))
+    # print(find_primes(100, 100, []))
+    print(prime_factorize(125))
 
