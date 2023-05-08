15d7eb7a35676d1d12e41517f16f148b87c8b4df
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-05-08 10:26:29 +0200

    Signal generating functions

--- list7/signal/generate.py
+++ list7/signal/generate.py
@@ -3,10 +3,10 @@ from typing import Callable
 
 
 class Signal():
-    def __init__(self, func: Callable[[float], float],
+    def __init__(self, func: Callable[[np.ndarray], np.ndarray],
                  sampling_freq: int,
                  signal_freq: int) -> None:
-        self._func = func
+        self._gen_func = func
         self._sampling_freq = sampling_freq
         self._signal_freq = signal_freq
         self._time_interval = 1/self._sampling_freq
@@ -18,9 +18,14 @@ class Signal():
         self._freq_domain = np.linspace(0,
                                         (self.number_of_sample - 1)*self._freq_interval,
                                         self.number_of_samples)
-        
+        self.signal = np.empty(self._time_domain.size)
+        self.transform = np.empty(self._freq_domain.size)
 
-        
+    def generate(self):
+        self.signal = self._gen_func(self._signal_freq*self._time_domain)
+
+    def make_fft(self):
+        self.transform = np.fft.fft(self.signal)
 
 
 class CreateSignal():
@@ -44,21 +49,21 @@ class CreateSignal():
         self._value_axis = value_axis
 
         if not func_str:
-            self._func = f'{A[0]}*np.sin({a[0]}*t) + {B[0]}*np.cos({b[0]}*t)'
+            self._gen_func = f'{A[0]}*np.sin({a[0]}*t) + {B[0]}*np.cos({b[0]}*t)'
         else:
-            self._func = func_str
+            self._gen_func = func_str
 
         for i in range(1, len(A)):
-            self._func += f' + {A[i]}*np.sin({a[i]}*t)'
+            self._gen_func += f' + {A[i]}*np.sin({a[i]}*t)'
 
         for i in range(1, len(B)):
-            self._func += f' + {B[i]}*np.cos({b[i]}*t)'
+            self._gen_func += f' + {B[i]}*np.cos({b[i]}*t)'
 
-        self._lambda_func = lambda t: eval(self._func)
+        self._lambda_func = lambda t: eval(self._gen_func)
         # self._lambda_func = lambda t: 3*np.sin(2*t) + 2*np.cos(3*t)
 
     def __str__(self) -> str:
-        return self._func
+        return self._gen_func
 
     # def generate_array(self, start=0, stop=20, step=0.1, save=True):
     #     arr = np.empty(int((stop-start)/step))
