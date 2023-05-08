760999dcc4238580487d5fc21aa60d40a0af527c
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-05-08 12:50:31 +0200

    filtering improved

--- list7/signal/filter.py
+++ list7/signal/filter.py
@@ -1,28 +1,34 @@
 import numpy as np
+from generate import Signal
 
 
 class Filter():
     def __init__(self) -> None:
         self._signal = None
+        self._signal_values = None
         self._signal_fft = None
+        self._time_domain = None
+        self._freq_domain = None
 
     @property
     def signal(self):
         return self._signal
 
     @signal.setter
-    def signal(self, new_value: np.ndarray):
+    def signal(self, new_value: Signal):
         self._signal = new_value
-        self._signal_fft = np.fft.fft(self._signal)
+        self._time_domain, self._signal_values = self._signal.generate()
+        self._freq_domain, self._signal_fft = self._signal.make_fft()
 
     @signal.getter
     def signal(self):
-        return self._signal
+        return self._signal_values
 
-    def out(self, frequencies: list):
-        # TODO: znajdz lepszy sposob na reprezentacje czestotliwosci do
-        # usuniecia
-
-        # frequencies: numery indeksow w do usuniecia
-        self._signal_fft = np.delete(self._signal_fft, frequencies)
-        self._signal = np.fft.ifft(self._signal_fft)
+    def out(self, frequencies: list[float]):
+        mask = []
+        for i in range(len(self._freq_domain)):
+            if self._freq_domain[i] in frequencies:
+                mask.append(False)
+            else:
+                mask.append(True)
+        return self._freq_domain[mask], self._signal_fft[mask]
