7953b4ab37c237c664c454931959fa57554a46b1
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-05-15 22:06:25 +0200

    hash almost working

--- list8/list8/hash_dict.py
+++ list8/list8/hash_dict.py
@@ -21,7 +21,7 @@ class HashTable:
         #         break
 
         self._content = [None for _ in range(self.size)]
-        self._support = [None for _ in range(self.size)]
+        # self._support = [None for _ in range(self.size)]
 
     def generate_hash(self, key, iteration=0) -> int:
         # key = hash(key)
@@ -30,7 +30,21 @@ class HashTable:
         ) % self.size
         return int(temp)
 
-    def insert_item(self, index, value):
+    def key(self, target):
+        target = str(target)
+        temp = ""
+        for symbol in target:
+            temp += str(ord(symbol))
+        return int(temp)
+
+    def key_gen(self, r: Robot, target):
+        temp = ""
+        for symbol in str(dict(list(inspect.getmembers(r)))[target]):
+            temp += str(ord(symbol))
+        return int(temp)
+
+    def insert_item(self, value, field):
+        index = self.key_gen(value, field)
         i = 0
         while i != self.size:
             hashed_index = self.generate_hash(index, i)
@@ -39,20 +53,21 @@ class HashTable:
                 or self._content[hashed_index] == "DEL"
             ):
                 self._content[hashed_index] = value
-                self._support[hashed_index] = index
+                # self._support[hashed_index] = index
                 break
             else:
                 i += 1
             # if i > self.size:
             #     raise MemoryError()
 
-    def search_item(self, index):
+    def search_item(self, value, field):
+        index = self.key(value)
         i = 0
         flag = True
         while flag:
             hashed_index = self.generate_hash(index, i)
-            if self._support[hashed_index] == index:
-                # return hashed_index
+            # if self._support[hashed_index] == index:
+            if self.key_gen(self._content[hashed_index], field) == index:
                 return self._content[hashed_index]
             else:
                 i += 1
@@ -61,33 +76,33 @@ class HashTable:
         return None
 
 
-def key_gen(r: Robot, target):
-    temp = ""
-    for symbol in str(dict(list(inspect.getmembers(r)))[target]):
-        temp += str(ord(symbol))
-    return int(temp)
+# def key(target):
+#     target = str(target)
+#     temp = ""
+#     for symbol in target:
+#         temp += str(ord(symbol))
+#     return int(temp)
 
 
-def key(target):
-    target = str(target)
-    temp = ""
-    for symbol in target:
-        temp += str(ord(symbol))
-    return int(temp)
+# def key_gen(r: Robot, target):
+#     temp = ""
+#     for symbol in str(dict(list(inspect.getmembers(r)))[target]):
+#         temp += str(ord(symbol))
+#     return int(temp)
 
 
 if __name__ == "__main__":
-    r = Robot("ACD", 256.0, 89, 0)
-    h = HashTable(max_size=4, alpha=0.5)
+    # r = Robot("ACD", 256.0, 89, 0)
+    # h = HashTable(max_size=4, alpha=0.5)
 
-    key_p = key_gen(r, "price")
-    print(f"Key: {key_p}")
+    # key_p = key_gen(r, "price")
+    # print(f"Key: {key_p}")
 
-    d = Robot("D", 256.0, 9, 1)
+    # d = Robot("D", 256.0, 9, 1)
 
-    h.insert_item(key_p, r)
-    h.insert_item(key_gen(d, "price"), d)
-    print(h.search_item(key(256.0)))
+    # h.insert_item(key_p, r)
+    # h.insert_item(key_gen(d, "price"), d)
+    # print(h.search_item(key(256.0)))
 
     # TODO: implemnt k(x) <-- key generating (from values function)
     #         k(x) = x
@@ -95,3 +110,12 @@ if __name__ == "__main__":
     # print(h.search_item(484846484848))
     # h.insert_item(565656556)
     # print(h.search_item(565656556))
+
+    r = Robot("ACD", 256.0, 89, 0)
+    h = HashTable(max_size=4, alpha=0.5)
+
+    d = Robot("D", 256.0, 9, 1)
+
+    h.insert_item(r, "price")
+    h.insert_item(d, "price")
+    print(h.search_item(256.0, "price"))
