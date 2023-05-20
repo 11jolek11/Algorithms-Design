7c4d1ba85e9e37b31d6783c365ec69ba79d7f566
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-05-20 21:18:58 +0200

    counting sort implementation, require small corrections to integrate with robots

--- list9/list9/counting_sort.py
+++ list9/list9/counting_sort.py
@@ -37,3 +37,73 @@
 
 # sortedArray = countingSort(inputArray)
 # print("Counting sort result = ", sortedArray)
+import numpy as np
+
+
+# def counting_sort(arr, k):
+#     C = np.zeros(k)
+#     B = np.zeros(len(arr))
+#     # print(C)
+#     # print(B)
+#     for i in range(len(arr)-1):
+#         # print(arr[i])
+#         C[arr[i]-1] += 1
+#     for i in range(1, len(arr)):
+#         C[i] += C[i-1]
+#     for i in range(len(arr)-1, -1, -1):
+#         B[int(C[arr[i]-1])] = arr[i]
+#         # print(B[int(C[arr[i]-1])])
+#         # print(arr[i])
+#         C[arr[i]-1] -= 1
+#         print(C[arr[i]-1])
+#     return B
+
+# def counting_sort(arr, max_value):
+#     count = [0] * (max_value + 1)
+#     output = [0] * len(arr)
+
+#     for num in arr:
+#         count[num] += 1
+
+#     for i in range(1, max_value + 1):
+#         count[i] += count[i - 1]
+
+#     for i in range(len(arr) - 1, -1, -1):
+#         num = arr[i]
+#         output[count[num] - 1] = num
+#         count[num] -= 1
+
+#     return output
+from robots import Table, Robot
+
+
+def counting_sort(arr, max_value):
+    count = [0] * (max_value + 1)
+    output = [0] * len(arr)
+
+    # for num in arr:
+    #     count[num] += 1
+    for num in range(len(arr)):
+        count[getattr(arr[num], "robot_range")] += 1
+
+    for i in range(1, max_value + 1):
+        count[i] += count[i - 1]
+
+    for i in range(len(arr) - 1, -1, -1):
+        # num = arr[i]
+        num = getattr(arr[i], "robot_range")
+        output[count[num] - 1] = num
+        count[num] -= 1
+
+    return output
+
+if __name__ == "__main__":
+    # arr = [4, 2, 2, 8, 3, 3, 1]
+    # sorted_arr = counting_sort(arr, 8)
+    # print(sorted_arr)
+    t = Table()
+    t.fill(5)
+    t.show()
+    t.frame = counting_sort(t.frame, 100)
+    t.show()
+
