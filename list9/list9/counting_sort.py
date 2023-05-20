652a6eefc83866f0c262536d221eecc52d6dec8b
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-05-20 22:55:14 +0200

    counting sort for robots and couting sort for numbers added

--- list9/list9/counting_sort.py
+++ list9/list9/counting_sort.py
@@ -1,79 +1,3 @@
-# def countingSort(inputArray):
-#     # Find the maximum element in the inputArray
-#     maxElement = max(inputArray)
-
-#     countArrayLength = maxElement+1
-
-#     # Initialize the countArray with (max+1) zeros
-#     countArray = [0] * countArrayLength
-
-#     # Step 1 -> Traverse the inputArray and increase 
-#     # the corresponding count for every element by 1
-#     for el in inputArray:
-#         countArray[el] += 1
-
-#     # Step 2 -> For each element in the countArray, 
-#     # sum up its value with the value of the previous 
-#     # element, and then store that value 
-#     # as the value of the current element
-#     for i in range(1, countArrayLength):
-#         countArray[i] += countArray[i-1]
-
-#     # Step 3 -> Calculate element position
-#     # based on the countArray values
-#     outputArray = [0] * len(inputArray)
-#     i = len(inputArray) - 1
-#     while i >= 0:
-#         currentEl = inputArray[i]
-#         countArray[currentEl] -= 1
-#         newPosition = countArray[currentEl]
-#         outputArray[newPosition] = currentEl
-#         i -= 1
-
-#     return outputArray
-
-# inputArray = [2,2,0,6,1,9,9,7]
-# print("Input array = ", inputArray)
-
-# sortedArray = countingSort(inputArray)
-# print("Counting sort result = ", sortedArray)
-import numpy as np
-
-
-# def counting_sort(arr, k):
-#     C = np.zeros(k)
-#     B = np.zeros(len(arr))
-#     # print(C)
-#     # print(B)
-#     for i in range(len(arr)-1):
-#         # print(arr[i])
-#         C[arr[i]-1] += 1
-#     for i in range(1, len(arr)):
-#         C[i] += C[i-1]
-#     for i in range(len(arr)-1, -1, -1):
-#         B[int(C[arr[i]-1])] = arr[i]
-#         # print(B[int(C[arr[i]-1])])
-#         # print(arr[i])
-#         C[arr[i]-1] -= 1
-#         print(C[arr[i]-1])
-#     return B
-
-# def counting_sort(arr, max_value):
-#     count = [0] * (max_value + 1)
-#     output = [0] * len(arr)
-
-#     for num in arr:
-#         count[num] += 1
-
-#     for i in range(1, max_value + 1):
-#         count[i] += count[i - 1]
-
-#     for i in range(len(arr) - 1, -1, -1):
-#         num = arr[i]
-#         output[count[num] - 1] = num
-#         count[num] -= 1
-
-#     return output
 from robots import Table, Robot
 
 
@@ -81,8 +5,24 @@ def counting_sort(arr, max_value):
     count = [0] * (max_value + 1)
     output = [0] * len(arr)
 
-    # for num in arr:
-    #     count[num] += 1
+    for num in arr:
+        count[num] += 1
+
+    for i in range(1, max_value + 1):
+        count[i] += count[i - 1]
+
+    for i in range(len(arr) - 1, -1, -1):
+        num = arr[i]
+        output[count[num] - 1] = num
+        count[num] -= 1
+
+    return output
+
+
+def counting_sort_robot(arr, max_value):
+    count = [0] * (max_value + 1)
+    output = [0] * len(arr)
+
     for num in range(len(arr)):
         count[getattr(arr[num], "robot_range")] += 1
 
@@ -90,10 +30,9 @@ def counting_sort(arr, max_value):
         count[i] += count[i - 1]
 
     for i in range(len(arr) - 1, -1, -1):
-        # num = arr[i]
-        num = getattr(arr[i], "robot_range")
-        output[count[num] - 1] = num
-        count[num] -= 1
+        num = arr[i]
+        output[count[getattr(arr[i], "robot_range")] - 1] = num
+        count[getattr(arr[i], "robot_range")] -= 1
 
     return output
 
@@ -101,9 +40,9 @@ if __name__ == "__main__":
     # arr = [4, 2, 2, 8, 3, 3, 1]
     # sorted_arr = counting_sort(arr, 8)
     # print(sorted_arr)
+
     t = Table()
     t.fill(5)
     t.show()
-    t.frame = counting_sort(t.frame, 100)
+    t.frame = counting_sort_robot(t.frame, 100)
     t.show()
-
