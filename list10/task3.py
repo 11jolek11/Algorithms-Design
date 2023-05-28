ee750734ae1926416a2fa2e79f4b33588c61ed1b
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-05-28 20:02:20 +0200

    important comment on task3 in this commit

--- list10/task3.py
+++ list10/task3.py
@@ -1,64 +1,178 @@
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
+
+#     def is_empty(self):
+#         return self.head is None
+
+#     def insert(self, key):
+#         new_node = [None, key, None]
+
+#         if self.is_empty():
+#             self.head = new_node
+#             self.tail = new_node
+#         else:
+#             new_node[0] = self.tail
+#             self.tail[2] = new_node
+#             self.tail = new_node
+
+#     def delete(self, key):
+#         current = self.head
+
+#         while current is not None:
+#             if current[1] == key:
+#                 prev_node = current[0]
+#                 next_node = current[2]
+
+#                 if prev_node is not None:
+#                     prev_node[2] = next_node
+#                 else:
+#                     self.head = next_node
+
+#                 if next_node is not None:
+#                     next_node[0] = prev_node
+#                 else:
+#                     self.tail = prev_node
+
+#                 return  # Found and deleted the node
+
+#             current = current[2]
+
+#     def search(self, key):
+#         current = self.head
+
+#         while current is not None:
+#             if current[1] == key:
+#                 return current
+
+#             current = current[2]
+
+#         return None
+
+#     def display(self):
+#         current = self.head
+
+#         while current is not None:
+#             print(current[1], end=" ")
+#             current = current[2]
+
+#         print()
+
+
+# # Example usage:
+# my_list = LinkedList()
+
+# my_list.insert(5)
+# my_list.insert(10)
+# my_list.insert(15)
+# my_list.insert(20)
+
+# my_list.display()  # Output: 5 10 15 20
+
+# my_list.delete(5)
+# # my_list.delete(10)
+# # my_list.delete(15)
+
+# my_list.display()  # Output: 5 20
+
+# # node = my_list.search(20)
+# # if node is not None:
+# #     print("Node found!")
+# # else:
+# #     print("Node not found!")
+
+
 class LinkedList:
     def __init__(self):
-        # Wzorzec głowy
-        self.head = [None, None, None]
-        # Head pointer to nie wartownik!
-        self.head_pointer = None
-        # self.content = [0 for _ in range(10)]
-        self.content = []
+        self.table = {}
+        self.head = None
+        self.tail = None
+        self.index = 0
 
     def is_empty(self):
-        return len(self.content) == 0
-        # return self.head is [None, None, None]
+        return self.head is None
 
     def insert(self, key):
         new_node = [None, key, None]
-        if self.is_empty():
-            self.head = new_node
-            self.content.append(self.head)
-            self.head_pointer = 0
-        else:
-            current_node = self.content[self.head_pointer]
-            while current_node[2] is not None:
-                current_node = self.content[current_node[2]]
-            # current_node[2] = new_node
-            # new_node[0] = current_node
 
-    def search(self, key):
-        current_node = self.head
-        while current_node is not None:
-            if current_node[1] == key:
-                return current_node
-            current_node = current_node[2]
-        return None
+        if self.is_empty():
+            self.head = self.index
+            self.tail = self.index
+        else:
+            new_node[0] = self.tail
+            self.table[self.tail][2] = self.index
+            self.tail = self.index
+
+        self.table[self.index] = new_node
+        self.index += 1
 
     def delete(self, key):
-        node_to_delete = self.search(key)
-        if node_to_delete is None:
-            return
-        if node_to_delete[0] is None:
-            self.head = node_to_delete[2]
-        else:
-            node_to_delete[0][2] = node_to_delete[2]
-            if node_to_delete[2] is not None:
-                node_to_delete[2][0] = node_to_delete[0]
+        current_index = self.head
+
+        while current_index is not None:
+            current_node = self.table[current_index]
+
+            if current_node[1] == key:
+                prev_index = current_node[0]
+                next_index = current_node[2]
+
+                if prev_index is not None:
+                    self.table[prev_index][2] = next_index
+                else:
+                    self.head = next_index
+
+                if next_index is not None:
+                    self.table[next_index][0] = prev_index
+                else:
+                    self.tail = prev_index
+
+                del self.table[current_index]  # Remove node from table
+                return  # Found and deleted the node
+
+            current_index = current_node[2]
+
+    def search(self, key):
+        current_index = self.head
+
+        while current_index is not None:
+            current_node = self.table[current_index]
+
+            if current_node[1] == key:
+                return current_node
+
+            current_index = current_node[2]
+
+        return None
 
     def display(self):
-        current_node = self.head
-        while current_node is not None:
+        current_index = self.head
+
+        while current_index is not None:
+            current_node = self.table[current_index]
             print(current_node[1], end=" ")
-            current_node = current_node[2]
+            current_index = current_node[2]
+
         print()
 
+
 # Example usage:
-linked_list = LinkedList()
-linked_list.insert(10)
-linked_list.insert(20)
-linked_list.insert(30)
-linked_list.display()  # Output: 10 20 30
+my_list = LinkedList()
 
-node = linked_list.search(20)
-print(node)  # Output: [None, 20, [None, 30, None]]
+my_list.insert(5)
+my_list.insert(10)
+my_list.insert(15)
+my_list.insert(20)
 
-linked_list.delete(20)
-linked_list.display()  # Output: 10 30
+my_list.display()  # Output: 5 10 15 20
+
+my_list.delete(10)
+my_list.delete(15)
+
+my_list.display()  # Output: 5 20
+
+node = my_list.search(20)
+if node is not None:
+    print("Node found!")
+else:
+    print("Node not found!")
